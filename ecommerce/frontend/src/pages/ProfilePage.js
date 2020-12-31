3264db46f7b43ffc0b245cf851a4a58ee4512732
Igor Hristov <58328456+igorhristov@users.noreply.github.com>
2020-12-31 19:49:13 +0100

    Update User Profile

--- ecommerce/frontend/src/pages/ProfilePage.js
+++ ecommerce/frontend/src/pages/ProfilePage.js
@@ -3,7 +3,10 @@ import { Form, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { getUserDetails } from "../redux/actions/userActions";
+import {
+  getUserDetails,
+  updateUserProfile,
+} from "../redux/actions/userActions";
 
 const ProfilePage = ({ location, history }) => {
   const [name, setName] = useState("");
@@ -19,6 +22,9 @@ const ProfilePage = ({ location, history }) => {
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
+
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const { success } = userUpdateProfile;
 
   useEffect(() => {
     if (!userInfo) {
@@ -40,6 +46,7 @@ const ProfilePage = ({ location, history }) => {
       setMessage("Password do not match");
     } else {
       // disptach Update Profile
+      dispatch(updateUserProfile({ id: user._id, name, email, password }));
     }
   };
 
@@ -49,6 +56,7 @@ const ProfilePage = ({ location, history }) => {
         <h2>User Profile</h2>
         {message && <Message variant="danger">{message}</Message>}
         {error && <Message variant="danger">{error}</Message>}
+        {success && <Message variant="success">Profile Updated</Message>}
         {loading && <Loader />}
 
         <Form onSubmit={submitHandler}>
--- ecommerce/frontend/src/redux/actions/userActions.js
+++ ecommerce/frontend/src/redux/actions/userActions.js
@@ -9,6 +9,10 @@ import {
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
   USER_DETAILS_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_RESET,
 } from "../constants/userConstants";
 
 // user LOGIN action
@@ -115,10 +119,52 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
       type: USER_DETAILS_SUCCESS,
       payload: response,
     });
-    
   } catch (error) {
     dispatch({
       type: USER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+// user USER DETAILS action
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_UPDATE_PROFILE_REQUEST,
+    });
+
+    const url = `/api/users/profile`;
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+      body: JSON.stringify(user),
+    }).then((data) => data.json());
+
+    dispatch({
+      type: USER_UPDATE_PROFILE_SUCCESS,
+      payload: response,
+    });
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: response,
+    });
+    localStorage.setItem('userInfo', JSON.stringify(response))
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATE_PROFILE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
