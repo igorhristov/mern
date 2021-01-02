3e3d5401b647b66c676e53c96c2a30f4c3e5aaeb
Igor Hristov <58328456+igorhristov@users.noreply.github.com>
2021-01-02 17:13:52 +0100

    Eshop Create and Save Product

--- eshop/backend/controllers/productController.js
+++ eshop/backend/controllers/productController.js
@@ -1,8 +1,18 @@
+const Product = require("../models/product");
+
+//#method
+//#path /api/v1/product/new
+//#desc Create new product
+
+exports.newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({ success: true, product });
+};
+
 exports.getProducts = (req, res, next) => {
-    
   res.status(200).json({
     success: true,
     message: "this route will sho all products in database",
   });
-
 };
