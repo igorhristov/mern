c8c845545d9f9a76552b0e9c71915ebaf2c86788
Igor Hristov <58328456+igorhristov@users.noreply.github.com>
2021-01-03 18:06:50 +0100

    production vs Development errors and catching async Errors

--- eshop/backend/controllers/productController.js
+++ eshop/backend/controllers/productController.js
@@ -1,19 +1,20 @@
 const Product = require("../models/product");
 
 const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 
 // #опис        Направи нов производ
 // #патека      POST /api/v1/admin/product/new
 // #пристап     ADMIN
-exports.newProduct = async (req, res, next) => {
+exports.newProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({ success: true, product });
-};
+});
 
 // #опис        Земи ги сите производи
 // #патека      GET /api/v1/products
 // #пристап     Јавен
-exports.getProducts = async (req, res, next) => {
+exports.getProducts = catchAsyncErrors(async (req, res, next) => {
   const products = await Product.find();
 
   res.status(200).json({
@@ -21,12 +22,12 @@ exports.getProducts = async (req, res, next) => {
     count: products.length,
     products,
   });
-};
+});
 
 // #опис        Земи ги деталите за еден производ
 // #патека      GET /api/v1/product/:id
 // #пристап     Јавен
-exports.getSingleProduct = async (req, res, next) => {
+exports.getSingleProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   // if (!product) {
@@ -41,18 +42,15 @@ exports.getSingleProduct = async (req, res, next) => {
   }
 
   res.status(200).json({ success: true, product });
-};
+});
 
 // #опис        Update на производ
 // #патека      PUT /api/v1/admin/product/:id
 // #пристап     ADMIN
-exports.updateProduct = async (req, res, next) => {
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
   if (!product) {
-    return res.status(404).json({
-      success: false,
-      message: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
 
   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
@@ -65,19 +63,16 @@ exports.updateProduct = async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
 
 // #опис        Бришење на производ
 // #патека      PUT /api/v1/admin/product/:id
 // #пристап     ADMIN
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(404).json({
-      success: false,
-      message: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
 
   await product.remove();
@@ -86,4 +81,4 @@ exports.deleteProduct = async (req, res, next) => {
     success: true,
     message: "Product is deleted",
   });
-};
+});
