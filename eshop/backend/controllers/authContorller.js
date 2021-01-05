54a3581f2fa8536f161e83dbb3d87f23e39e0d08
Igor Hristov <58328456+igorhristov@users.noreply.github.com>
2021-01-05 13:50:15 +0100

    Eshop Reset New Password & Handle JWT wrong and expired token error

--- eshop/backend/controllers/authContorller.js
+++ eshop/backend/controllers/authContorller.js
@@ -3,7 +3,8 @@ const User = require("../models/user");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const sendToken = require("../utils/jwtToken");
-const sendEmail = require('../utils/sendEmail')
+const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
 
 // #опис        Register a user
 // #патека      POST /api/v1/register
@@ -94,6 +95,45 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
 
     return next(new ErrorHandler(error.message, 500));
   }
+});
+
+// #опис        Reset Password
+// #патека      PUT /api/v1/password/reset/:token
+// #пристап     Privaten
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+  // Hash URL token
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(
+      new ErrorHandler(
+        "Password reset token is invalid or has been expired",
+        400
+      )
+    );
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler("Password does not match", 400));
+  }
+
+  // Setup new password
+  user.password = req.body.password;
+
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
+
+  sendToken(user, 200, res);
 });
 
 // #опис        Logout user
