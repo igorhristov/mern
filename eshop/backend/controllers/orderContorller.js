aef48cabed898eed1b0ccc392081146426d665c8
Igor Hristov <58328456+igorhristov@users.noreply.github.com>
2021-01-07 00:06:10 +0100

    Eshop orders for ADMINS get all order, update procces order and delte order

--- eshop/backend/controllers/orderContorller.js
+++ eshop/backend/controllers/orderContorller.js
@@ -66,3 +66,69 @@ exports.myOrders = catchAsyncErrors(async (req, res, next) => {
     orders,
   });
 });
+
+// #опис        Get all orders
+// #патека      GET /api/v1/admin/orders
+// #пристап     ADMIN
+exports.allOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  let totalAmount = 0;
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
+
+  res.status(200).json({
+    success: true,
+    totalAmount,
+    orders,
+  });
+});
+
+// #опис        Update / Process order
+// #патека      PUT /api/v1/admin/order/:id
+// #пристап     ADMIN
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHandler("You have already delivered this order", 400));
+  }
+
+  order.orderItems.forEach(async (item) => {
+    await updateStock(item.product, item.quantity);
+  });
+
+  order.orderStatus = req.body.status;
+  order.deliveredAt = Date.now();
+
+  await order.save();
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+  product.stock = product.stock - quantity;
+
+  await product.save({ validateBeforeSave: false });
+}
+
+// #опис        Избриши порачка
+// #патека      DELETE /api/v1/admin/order/:id
+// #пристап     ADMIN
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("No Order found with this ID", 404));
+  }
+
+  await order.remove();
+
+  res.status(200).json({
+    success: true,
+  });
+});
--- eshop/backend/routes/order.js
+++ eshop/backend/routes/order.js
@@ -5,6 +5,9 @@ const {
   newOrder,
   getSingleOrder,
   myOrders,
+  allOrders,
+  updateOrder,
+  deleteOrder,
 } = require("../controllers/orderContorller");
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
@@ -13,5 +16,14 @@ router.route("/order/new").post(isAuthenticatedUser, newOrder);
 
 router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), allOrders);
+
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
 
 module.exports = router;
