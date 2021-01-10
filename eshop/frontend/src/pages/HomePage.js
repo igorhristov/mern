931f8f154ffbe7d58317c6fa072b47dd5c81c34d
Igor Hristov <58328456+igorhristov@users.noreply.github.com>
2021-01-11 00:41:14 +0100

    Eshop add Pagination

--- eshop/frontend/src/pages/HomePage.js
+++ eshop/frontend/src/pages/HomePage.js
@@ -3,6 +3,7 @@ import { Row } from "react-bootstrap";
 import MetaData from "../components/layout/MetaData";
 import Product from "../components/product/Product";
 import Loader from "../components/layout/Loader";
+import Pagination from "react-js-pagination";
 
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../redux/actions/productActions";
@@ -11,10 +12,12 @@ import { useAlert } from "react-alert";
 import "./homepage.css";
 
 const HomePage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
   const alert = useAlert();
   const dispatch = useDispatch();
 
-  const { loading, products, error, productsCount } = useSelector(
+  const { loading, products, error, productsCount, resPerPage } = useSelector(
     (state) => state.products
   );
 
@@ -23,8 +26,12 @@ const HomePage = () => {
       return alert.error(error);
     }
 
-    dispatch(getProducts());
-  }, [dispatch, alert, error]);
+    dispatch(getProducts(currentPage));
+  }, [dispatch, alert, error, currentPage]);
+
+  function setCurrentPageNo(pageNumber) {
+    setCurrentPage(pageNumber);
+  }
 
   return (
     <>
@@ -42,6 +49,23 @@ const HomePage = () => {
                 ))}
             </Row>
           </section>
+
+          {resPerPage <= productsCount && (
+            <div className="d-flex justify-content-center mt-5">
+              <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={resPerPage}
+                totalItemsCount={productsCount}
+                onChange={setCurrentPageNo}
+                nextPageText={"Next"}
+                prevPageText={"Next"}
+                firstPageText={"First"}
+                lastPageText={"Last"}
+                itemClass="page-item"
+                linkClass="page-link"
+              />
+            </div>
+          )}
         </>
       )}
     </>
