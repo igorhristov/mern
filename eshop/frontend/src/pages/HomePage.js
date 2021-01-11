99ceaf18be6e8d95c6bbf50e0047bc15c14ad07b
Igor Hristov <58328456+igorhristov@users.noreply.github.com>
2021-01-11 23:39:23 +0100

    Eshop apply Filter by Price

--- eshop/frontend/src/pages/HomePage.js
+++ eshop/frontend/src/pages/HomePage.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+import "./homepage.css";
 import MetaData from "../components/layout/MetaData";
 import Product from "../components/product/Product";
 import Loader from "../components/layout/Loader";
@@ -9,10 +12,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../redux/actions/productActions";
 import { useAlert } from "react-alert";
 
-import "./homepage.css";
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range);
 
 const HomePage = ({ match }) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([1, 100000]);
 
   const alert = useAlert();
   const dispatch = useDispatch();
@@ -28,8 +33,8 @@ const HomePage = ({ match }) => {
       return alert.error(error);
     }
 
-    dispatch(getProducts(keyword, currentPage));
-  }, [dispatch, alert, error, keyword, currentPage]);
+    dispatch(getProducts(keyword, currentPage, price));
+  }, [dispatch, alert, error, keyword, currentPage, price]);
 
   function setCurrentPageNo(pageNumber) {
     setCurrentPage(pageNumber);
@@ -45,10 +50,45 @@ const HomePage = ({ match }) => {
           <h1 id="products_heading">latest products</h1>
           <section id="products">
             <Row>
-              {products &&
+              {keyword ? (
+                <>
+                  <Col md={3} className="my-5">
+                    <div className="px-5">
+                      <Range
+                        marks={{ 1: `$1`, 100000: `$100000` }}
+                        min={1}
+                        max={100000}
+                        defaultValue={[1, 100000]}
+                        tipFormatter={(value) => `$${value}`}
+                        tipProps={{
+                          placement: "top",
+                          visible: true,
+                        }}
+                        value={price}
+                        onChange={(price) => setPrice(price)}
+                      />
+                    </div>
+                  </Col>
+
+                  <Col md={9}>
+                    <Row>
+                      {products &&
+                        products.map((product) => (
+                          <Product
+                            key={product._id}
+                            product={product}
+                            col={4}
+                          />
+                        ))}
+                    </Row>
+                  </Col>
+                </>
+              ) : (
+                products &&
                 products.map((product) => (
-                  <Product key={product._id} product={product} />
-                ))}
+                  <Product key={product._id} product={product} col={3} />
+                ))
+              )}
             </Row>
           </section>
 
