2fd5b6c3471037618dec3af64fc00a8c2da5a920
Igor Hristov <58328456+igorhristov@users.noreply.github.com>
2021-01-16 15:14:48 +0100

    Eshop Load User

--- eshop/frontend/src/redux/actions/userActions.js
+++ eshop/frontend/src/redux/actions/userActions.js
@@ -7,6 +7,9 @@ import {
   REGISTER_USER_REQUEST,
   REGISTER_USER_SUCCESS,
   REGISTER_USER_FAIL,
+  LOAD_USER_REQUEST,
+  LOAD_USER_SUCCESS,
+  LOAD_USER_FAIL,
   CLEAR_ERRORS,
 } from "../constants/userConstants";
 
@@ -41,9 +44,8 @@ export const login = (email, password) => async (dispatch) => {
 // Register User
 export const register = (userData) => async (dispatch) => {
   try {
-
     dispatch({ type: REGISTER_USER_REQUEST });
-    
+
     const config = {
       headers: {
         "Content-Type": "multipart/form-data",
@@ -56,16 +58,32 @@ export const register = (userData) => async (dispatch) => {
       type: REGISTER_USER_SUCCESS,
       payload: data.user,
     });
-
   } catch (error) {
-
     dispatch({
       type: REGISTER_USER_FAIL,
       payload: error.response.data.message,
     });
-
   }
+};
 
+// Load User
+export const loadUser = () => async (dispatch) => {
+  try {
+    dispatch({ type: LOAD_USER_REQUEST });
+
+    const { data } = await axios.get("/api/v1/me");
+
+    dispatch({
+      type: LOAD_USER_SUCCESS,
+      payload: data.user,
+    });
+    
+  } catch (error) {
+    dispatch({
+      type: LOAD_USER_FAIL,
+      payload: error.response.data.message,
+    });
+  }
 };
 
 // Clear Errors
