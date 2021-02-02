3ee2d245213ce434f0635a7794c7942db4c2c473
Igor Hristov <58328456+igorhristov@users.noreply.github.com>
2021-02-02 21:29:49 +0100

    MEAN Fetching Initial Posts

--- mean/frontend/backend/app.js
+++ mean/frontend/backend/app.js
@@ -2,13 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First Middleware");
-  next();
-});
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "adfadsfew",
+      title: "First Server Post",
+      content: "this is coming from server",
+    },
+    {
+      id: "sfae4asdf",
+      title: "Second Server Post",
+      content: "this is coming from server 2",
+    },
+    {
+      id: "kf6djhn,",
+      title: "Third Server Post",
+      content: "this is coming from server 3",
+    },
+  ];
 
-app.use((req, res, next) => {
-  res.send("Hello from express");
+  res.status(200).json({ message: "Posts fetched succesfully!", posts: posts });
 });
 
 module.exports = app;
