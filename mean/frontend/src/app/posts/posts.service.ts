903050ad2f837a21a9dcb96a1fa96864233319cd
Igor Hristov <58328456+igorhristov@users.noreply.github.com>
2021-02-03 20:10:20 +0100

    MEAN Transforming Response Data from _id to id

--- mean/frontend/src/app/posts/posts.service.ts
+++ mean/frontend/src/app/posts/posts.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http'
 import { Subject } from 'rxjs';
-
+import { map } from 'rxjs/operators'
 import { Post } from "./post.model";
 
 // this can be done inside modules inside app module inside providers
@@ -16,10 +16,20 @@ export class PostService {
 
   getPosts() {
     // return [...this.posts];
-    this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts').subscribe((postData) => {
-      this.posts = postData.posts;
-      this.postUpdated.next([...this.posts]);
-    })
+    this.http.get<{ message: string, posts: any }>('http://localhost:3000/api/posts')
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          }
+        })
+      }))
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
+        this.postUpdated.next([...this.posts]);
+      })
   }
   getPostUpdateListener() {
     return this.postUpdated.asObservable();
